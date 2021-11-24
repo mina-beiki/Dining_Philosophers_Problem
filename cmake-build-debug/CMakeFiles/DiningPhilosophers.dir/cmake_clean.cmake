7b5989125cbf962a491081130e3d7c5c9dd42e32
mina-beiki <mina_beiki@yahoo.com>
2021-11-24 18:00:01 +0330

    init

---
+++ cmake-build-debug/CMakeFiles/DiningPhilosophers.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DiningPhilosophers.dir/main.c.o"
+  "DiningPhilosophers"
+  "DiningPhilosophers.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/DiningPhilosophers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
